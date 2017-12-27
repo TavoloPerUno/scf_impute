43e05000e5ad3512d4c0680a9fef7b69206865ca
William <morelwmilliam@gmail.com>
2017-12-26 20:46:36 -0600

    RForest

--- scf_impute/impute.py
+++ scf_impute/impute.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import re
 from predictive_imputer import predictive_imputer
-from fancyimpute import BiScaler, KNN, NuclearNormMinimization, SoftImpute
+# from fancyimpute import BiScaler, KNN, NuclearNormMinimization, SoftImpute
 
 def rforest_impute(dct_data, dct_param):
     df_raw_data = dct_data['df_raw_data']
@@ -21,13 +21,13 @@ def xgboost_impute(dct_data, dct_param):
 
     return filled_in
 
-def knn_impute(dct_data, dct_param, k):
-    df_raw_data = dct_data['df_raw_data']
-    df_raw_data.replace('nan', np.nan)
-    filled_in = KNN(k=k).complete(df_raw_data)
-
-    df_filled_in = pd.DataFrame(data=filled_in, columns=df_raw_data.columns, index=df_raw_data.index)
-
-    return df_filled_in
+# def knn_impute(dct_data, dct_param, k):
+#     df_raw_data = dct_data['df_raw_data']
+#     df_raw_data.replace('nan', np.nan)
+#     filled_in = KNN(k=k).complete(df_raw_data)
+#
+#     df_filled_in = pd.DataFrame(data=filled_in, columns=df_raw_data.columns, index=df_raw_data.index)
+#
+#     return df_filled_in
 
 
